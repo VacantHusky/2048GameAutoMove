0901d0535497072cdbf08d8b56465d6a0617874d
tiger-wang <48857256+VacantHusky@users.noreply.github.com>
2019-09-30 21:01:46 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,13 @@
 class Config:
     DEBUG = False
     TESTING = False
-    # mysql+pymysql://user:password@host:port/database
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/myblog'
-
 
 class DevelopmentConfig(Config):
     DEBUG = True
     ENV = 'development'
 
-
 class ProductionConfig(Config):
-    DATABASE_URI = ''
-
+    pass
 
 class TestingConfig(Config):
     TESTING = True
